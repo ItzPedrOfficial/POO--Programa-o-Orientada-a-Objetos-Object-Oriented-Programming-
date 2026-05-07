65f0ace5bd5d778525f856f02139a968ce055a8d
ItzPedrOfficial <a107327@alunos.uminho.pt>
2026-05-07 16:19:04 +0100

    Elimineei um import nao usado em controlador.java

--- Control/Controlador.java
+++ Control/Controlador.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Set;
 
