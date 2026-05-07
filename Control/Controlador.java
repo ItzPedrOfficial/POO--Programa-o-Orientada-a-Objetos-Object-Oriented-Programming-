4e3d899f1776d1b316dad02d2568cfbc61220925
ItzPedrOfficial <a107327@alunos.uminho.pt>
2026-05-07 16:13:17 +0100

    a

--- Control/Controlador.java
+++ Control/Controlador.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class Controlador {
 
     private DomusControl modelo;
-    private String utilizadorAtual;
+    
 
     // -------------------------------------------------------------------------
     // Construtor
